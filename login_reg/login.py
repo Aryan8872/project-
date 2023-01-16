0f2d86e938178546e603fdb1c1f250e8e30c32e5
Aryan8872 <gamersimpact778@gmail.com>
2023-01-17 01:34:14 +0545

    database connection

--- login_reg/login.py
+++ login_reg/login.py
@@ -1,12 +1,15 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
+import sqlite3
+from tkinter import messagebox
+
 root=Tk()
 root.geometry("1920x1108")
 root.title("Municipal problem resolver")
 
 #background image
-bacg=ImageTk.PhotoImage(Image.open("login and reg/6.jpg"))
+bacg=ImageTk.PhotoImage(Image.open("login_reg/6.jpg"))
 bac=Label(root,image=bacg,height=1108,width=1920)
 bac.place(x=0,y=0)
 
@@ -21,33 +24,34 @@ login.place(x=110,y=30)
 
 
 #logo
-logo=ImageTk.PhotoImage(Image.open("login and reg/logo5.jpg"))
+logo=ImageTk.PhotoImage(Image.open("login_reg/logo5.jpg"))
 lg=Label(frame,image=logo,bg="purple",width=100,height=100)
 lg.place(x=250,y=80)
 
 
 #username
 un_label=Label(frame,text="USERNAME",bg="#040405",font=("Helvetica 10 bold"),fg="#4f4e4d")
-un=Entry(frame,font=("Helvetica 14 bold"),bg="#040405",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+un_entry=Entry(frame,font=("Helvetica 14 bold"),bg="#040405",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 un_line=Canvas(frame,width=300,height=2.0,bg="#bdb9b1",highlightthickness=0)
-un.place(x=200,y=200)
+un_entry.place(x=200,y=200)
 un_line.place(x=170,y=225)
 un_label.place(x=120,y=180)
 
-un_logo=ImageTk.PhotoImage(Image.open("login and reg/username.png"))
+un_logo=ImageTk.PhotoImage(Image.open("login_reg/username.png"))
 u=Label(frame,image=un_logo,bg="#040405",height=20,width=23)
 u.place(x=160,y=200)
 
 
 #password
 pw_label=Label(frame,text="PASSWORD",bg="#040405",font=("Helvetica 10 bold"),fg="#4f4e4d")
-pw=Entry(frame,font=("Helvetica 14 bold"),bg="#040405",highlightthickness=0,relief=FLAT,fg="#6b6a69",show="*")
+pw_entry=Entry(frame,font=("Helvetica 14 bold"),bg="#040405",highlightthickness=0,relief=FLAT,fg="#6b6a69",show="*")
 pw_line=Canvas(frame,width=300,height=2.0,bg="#bdb9b1",highlightthickness=0)
-pw.place(x=200,y=260)
+pw_entry.place(x=200,y=260)
 pw_line.place(x=170,y=280)
 pw_label.place(x=120,y=235)
 
-pw_logo=ImageTk.PhotoImage(Image.open("login and reg/password.png"))
+pw_logo=ImageTk.PhotoImage(Image.open("login_reg/password.png"))
+
 p=Label(frame,image=pw_logo,bg="#040405",height=20,width=23)
 p.place(x=160,y=255)
 
@@ -55,14 +59,6 @@ p.place(x=160,y=255)
 #forgot password
 fp=Button(frame,text="FORGOT PASSWORD?",font=("Helvetica 10 italic"),bg="black",fg="red",activebackground="black",bd=0)
 fp.place(x=280,y=290)
-
-def login_action():
-    pw.delete(0,END)
-    un.delete(0,END)
-
-#login button
-lb=Button(frame,text="LOGIN",bg="green",fg="white",font=("Helvetica 14 bold"),padx=50,command=login_action,cursor="hand2")
-lb.place(x=220,y=320)
 
 
 #question
@@ -72,14 +68,52 @@ ques.place(x=175,y=390)
 
 #create account
 crea=Button(frame,text="create one",font=("Arial 10 bold"),fg="white",bg="black")
-
-
 crea.place(x=390,y=389)
 
 
 #about us
 au=Label(frame,text="ABOUT US",font=("Helvetica 10 bold"),bg="black",fg="limegreen")
 au.place(x=260,y=440)
+
+
+#===================================================================================================================================
+                             #DATABASE CONNECTION
+
+
+
+conn=sqlite3.connect('login.db')
+c=conn.cursor()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS login(
+    user_name text,
+    password string
+
+)""")
+
+
+
+def login_action():
+    conn = sqlite3.connect("login.db")
+    cursor = conn.cursor()
+
+    find_user = 'SELECT * FROM login WHERE user_name = ? and password = ?'
+    cursor.execute(find_user, [(un_entry.get()), (pw_entry.get())])
+
+    result = cursor.fetchall()
+    if result:
+        messagebox.showinfo("Success", 'Logged in Successfully.')
+    else:
+        messagebox.showerror("Failed", "Wrong Login details, please try again.")
+
+
+
+
+#login button
+login_b=Button(frame,text="LOGIN",bg="green",fg="white",font=("Helvetica 14 bold"),padx=50,command=login_action,cursor="hand2")
+login_b.place(x=220,y=320)
+
+
+
 
 
 
--- login_reg/registration.py
+++ login_reg/registration.py
@@ -1,15 +1,17 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
+import sqlite3
+
 root=Tk()
 root.geometry("1845x1070")
 root.title("Registration page")
 root.config(bg="black")
 
 #background image
-bacg=ImageTk.PhotoImage(Image.open("login and reg/bgreg2.jpg"))
-bac=Label(root,image=bacg,height=1070,width=1845)
-bac.place(x=0,y=0)
+root_bg=ImageTk.PhotoImage(Image.open("login_reg/bgreg2.jpg"))
+back=Label(root,image=root_bg,height=1070,width=1845)
+back.place(x=0,y=0)
 
 
 #title
@@ -24,9 +26,9 @@ title3.place(x=250,y=350)
 frame=LabelFrame(root,width=600,height=640,bg="black")
 frame.place(x=900,y=120)
 #frame bg
-bac=ImageTk.PhotoImage(Image.open("login and reg/r3.jpg"))
-back=Label(frame,image=bac,height=640,width=600)
-back.place(x=0,y=0)
+frame_bg=ImageTk.PhotoImage(Image.open("login_reg/r3.jpg"))
+bacg=Label(frame,image=frame_bg,height=640,width=600)
+bacg.place(x=0,y=0)
 
 #registration page label
 reg_label=Label(frame,text="REGISTRATION",fg="white",bg="black",font=("Helvetica 25 bold"))
@@ -37,68 +39,62 @@ page_label.place(x=255,y=45)
 #username label
 un_label=Label(frame,text="USERNAME",bg="#040405",font=("Helvetica 10 bold"),fg="white")
 
-un=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+un_ent=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 un_line=Canvas(frame,width=260,height=2.0,bg="#bdb9b1",highlightthickness=0)
 
-un.place(x=200,y=210)
+un_ent.place(x=200,y=210)
 un_line.place(x=190,y=234)
 un_label.place(x=160,y=190)
 
 #email
 email_label=Label(frame,text="E-MAIL",bg="#040405",font=("Helvetica 10 bold"),fg="white")
 
-email=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+email_ent=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 email_line=Canvas(frame,width=260,height=2.0,bg="#bdb9b1",highlightthickness=0)
 
-email.place(x=200,y=270)
+email_ent.place(x=200,y=270)
 email_line.place(x=190,y=294)
 email_label.place(x=160,y=250)
 
 #PASSWORD
 passwd_label=Label(frame,text="PASSWORD",bg="#040405",font=("Helvetica 10 bold"),fg="white")
 
-passwd=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+passwd_ent=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 passwd_line=Canvas(frame,width=260,height=2.0,bg="#bdb9b1",highlightthickness=0)
 
-passwd.place(x=50,y=330)
+passwd_ent.place(x=50,y=330)
 passwd_line.place(x=35,y=354)
 passwd_label.place(x=20,y=310)
 
 #RE-TYPE PASSWORD
 re_passwd_label=Label(frame,text="RE-TYPE PASSWORD",bg="#040405",font=("Helvetica 10 bold"),fg="white")
 
-re_passwd=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+re_passwd_entry=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 re_passwd_line=Canvas(frame,width=260,height=2.0,bg="#bdb9b1",highlightthickness=0)
 
-re_passwd.place(x=340,y=330)
+re_passwd_entry.place(x=340,y=330)
 re_passwd_line.place(x=325,y=354)
 re_passwd_label.place(x=300,y=310)
 
 #PHONE NUMBER
 phn_label=Label(frame,text="PHONE-NUMBER",bg="#040405",font=("Helvetica 10 bold"),fg="white")
 
-phn=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
+phn_entry=Entry(frame,font=("Helvetica 14 bold"),bg="gray11",highlightthickness=0,relief=FLAT,fg="#6b6a69")
 phn_line=Canvas(frame,width=260,height=2.0,bg="#bdb9b1",highlightthickness=0)
 
-phn.place(x=200,y=390)
+phn_entry.place(x=200,y=390)
 phn_line.place(x=190,y=414)
 phn_label.place(x=160,y=370)
 
-#register
-img=ImageTk.PhotoImage(Image.open("login and reg/button.png"))
-btn_lbl=Label(frame,image=img,bg="gray11")
-btn_lbl.place(x=200,y=520)
-register=Button(frame,text="REGISTER",fg="white",bg="limegreen",font=("Helvetica 15 bold"),activeforeground="limegreen",width=18,bd=0,cursor="hand2")
-register.place(x=221,y=532)
 
 #radio button
 gender=Label(frame,text="GENDER",bg="gray11",fg="cyan",font=("Helvetica 10 bold"))
 gender.place(x=160,y=430)
-gender1=Checkbutton(frame,text="male",bg="gray11",font=("Helvetica 10 bold"))
+gender1=Checkbutton(frame,text="male",bg="gray11",font=("Helvetica 10 bold"),variable="male")
 gender1.place(x=200,y=460)
-gender2=Checkbutton(frame,text="female",fg="limegreen",bg="gray11",font=("Helvetica 10 bold"))
+gender2=Checkbutton(frame,text="female",fg="limegreen",bg="gray11",font=("Helvetica 10 bold"),variable="female")
 gender2.place(x=280,y=460)
-gender3=Checkbutton(frame,text="other",fg="blue2",bg="gray11",font=("Helvetica 10 bold"))
+gender3=Checkbutton(frame,text="other",fg="blue2",bg="gray11",font=("Helvetica 10 bold"),variable="others")
 gender3.place(x=380,y=460)
 
 
@@ -109,6 +105,52 @@ text2=Button(frame,text="LOGIN",bg="gray11",fg="green",activebackground="gray11"
 text.place(x=220,y=600)
 text2.place(x=365,y=600)
 
-#radio button
+#===================================================================================================================================
+#===================================================================================================================================
+                               #database connnection
+                            
+
+conn=sqlite3.connect('registration.db')
+c=conn.cursor()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS register(
+    user_name text,
+    password string,
+    email string,
+    phone_num integer
+   
+
+)""")
+
+def add_rec():
+    conn=sqlite3.connect('registration.db')
+    c=conn.cursor()
+
+    c.execute("INSERT INTO register VALUES(:user_name, :password, :email, :phone_num )",{
+        'user_name':un_ent.get(),
+        'password':passwd_ent.get(),
+        'email':email_ent.get(),
+        'phone_num':phn_entry.get()
+        
+
+    })
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+
+
+
+
+#register
+img=ImageTk.PhotoImage(Image.open("login_reg/button.png"))
+btn_lbl=Label(frame,image=img,bg="gray11")
+btn_lbl.place(x=200,y=520)
+register=Button(frame,text="REGISTER",fg="white",bg="limegreen",font=("Helvetica 15 bold"),activeforeground="limegreen",width=18,bd=0,cursor="hand2",command=add_rec)
+register.place(x=221,y=532)
 
 root.mainloop()
