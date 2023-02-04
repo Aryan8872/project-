38570f6e782a01a8ab21a6fe5ab70a55c2b0d7fc
Kshitishneupane <kshitish.neupane@gmail.com>
2023-02-04 13:28:20 +0545

    Registration
    changes to registration

--- login_reg/registration.py
+++ login_reg/registration.py
@@ -1,128 +1,139 @@
 from tkinter import *
 import sqlite3
-from PIL import Image,ImageTk
 from tkinter import messagebox
 
+root=Tk()
+root.title("Municipal Problem Reporting")
+root.configure(background='white')
+root.geometry("1200x700")
+# root.iconbitmap('Capture.ico')
+root.configure(background='white')
 
-reg=Tk()
-reg.geometry("1845x1070")
-reg.title("Registration page")
-reg.config(bg="lavender")
+def open_login():
+    root.destroy()
+    import login
 
-   
-    #frame
-frame2=LabelFrame(reg,width=600,height=640,bg="lavender")
-frame2.place(x=924,y=100)
+#FRAMES 
+frame2=Frame(root,width=400,height=700,highlightbackground="skyblue", highlightthickness=1,background="skyblue")
+frame2.place(x=800,y=0)
+
+frame1=Frame(root,width=800,height=700,highlightbackground="white", highlightthickness=1,background="white")
+frame1.place(x=0,y=0)
+
+#labels
+registration=Label(frame1,text="Registration",font=('Ariel',35),fg="skyblue",background="white")
+registration.place(x=75,y=25)
+
+username=Label(frame1,text="User Name",font=('Ariel', 17),fg="skyblue",background="white")
+username.place(x=45,y=120)
+
+password=Label(frame1,text="Password",font=('Ariel', 17),fg="skyblue",background="white")
+password.place(x=445,y=110)
+
+phn=Label(frame1,text="Mobile Number",font=('Ariel', 17),fg="skyblue",background="white")
+phn.place(x=45,y=220)
+
+email1=Label(frame1,text="E-Mail Address",font=('Ariel', 17),fg="skyblue",background="white")
+email1.place(x=45,y=330)
+
+add=Label(frame1,text="Address",font=('Ariel', 17),fg="skyblue",background="white")
+add.place(x=445,y=330)
 
 
-photo_frame=LabelFrame(reg,width=900,height=900)
-photo_frame.place(x=0,y=0)
-photo=ImageTk.PhotoImage(Image.open("123.jpg"))
-bg=Label(photo_frame,image=photo)
-bg.place(x=0,y=0)
-    
-   
-    #registration page label
-reg_label=Label(reg,text="REGISTRATION",fg="black",bg="lavender",font=("Helvetica 25 bold"))
-reg_label.place(x=1100,y=0)
-page_label=Label(reg,text="PAGE",bg="lavender",fg="black",font=("Helvetica 25 bold"))
-page_label.place(x=1175,y=45)
+Confirm=Label(frame1,text="Confirm Password",font=('Ariel', 17),fg="skyblue",background="white")
+Confirm.place(x=445,y=220)
 
-#first name
-first_name_label=Label(frame2,text="FIRST NAME",bg="lavender",font=("Helvetica 10 bold"),fg="black")
-first_name_ent=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="#6b6a69")
-first_name_line=Canvas(frame2,width=220,height=2.0,bg="black",highlightthickness=0)
+ward=Label(frame1,text="Ward Number",font=('Ariel', 17),fg="skyblue",background="white")
+ward.place(x=45,y=440)
 
-first_name_ent.place(x=40,y=150)
-first_name_line.place(x=40,y=174)
-first_name_label.place(x=10,y=130)
+calls=Label(frame2,text="Contact:",font=('Ariel', 25),fg="white",background="skyblue")
+calls.place(x=10,y=510)
 
-#lastname
-lastname_label=Label(frame2,text="LAST NAME",bg="lavender",font=("Helvetica 10 bold"),fg="black")
-lastname_ent=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="#6b6a69")
-lastname_line=Canvas(frame2,width=220,height=2.0,bg="black",highlightthickness=0)
+num=Label(frame2,text="9862186238",font=('Ariel', 25),fg="white",bg="skyblue")
+num.place(x=10,y=550)
 
-lastname_ent.place(x=350,y=150)
-lastname_line.place(x=350,y=174)
-lastname_label.place(x=310,y=130)
+#ENTRY BOXES
+usernam_entry=Entry(frame1,width=35,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+usernam_entry.place(x=58,y=165)
 
-    #username 
-un_label=Label(frame2,text="USERNAME",bg="lavender",font=("Helvetica 10 bold"),fg="black")
-un_ent=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="#6b6a69")
-un_line=Canvas(frame2,width=260,height=2.0,bg="black",highlightthickness=0)
+pass_entry=Entry(frame1,width=25,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+pass_entry.place(x=458,y=165)
 
-un_ent.place(x=200,y=210)
-un_line.place(x=190,y=234)
-un_label.place(x=160,y=190)
+confpass_entry=Entry(frame1,width=25,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+confpass_entry.place(x=458,y=275)
 
-    #email
-email_label=Label(frame2,text="E-MAIL",bg="lavender",font=("Helvetica 10 bold"),fg="black")
+entryemail=Entry(frame1,width=25,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+entryemail.place(x=58,y=385)
 
-email_ent=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="black")
-email_line=Canvas(frame2,width=260,height=2.0,bg="black",highlightthickness=0)
+addr_ent=Entry(frame1,width=25,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+addr_ent.place(x=458,y=385)
 
-email_ent.place(x=200,y=270)
-email_line.place(x=190,y=294)
-email_label.place(x=160,y=250)
+entryward=Entry(frame1,width=35,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+entryward.place(x=58,y=495)
 
-    #PASSWORD
-passwd_label=Label(frame2,text="PASSWORD",bg="lavender",font=("Helvetica 10 bold"),fg="black")
-passwd_ent=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="#6b6a69")
-passwd_line=Canvas(frame2,width=260,height=2.0,bg="black",highlightthickness=0)
 
-passwd_ent.place(x=50,y=330)
-passwd_line.place(x=35,y=354)
-passwd_label.place(x=20,y=310)
+phn_entry=Entry(frame1,width=25,highlightthickness = 0, borderwidth=0,font=('Ariel', 15),background="white")
+phn_entry.place(x=58,y=275)
+phn_entry.insert(0,"+977")
 
-    #RE-TYPE PASSWORD
-re_passwd_label=Label(frame2,text="RE-TYPE PASSWORD",bg="lavender",font=("Helvetica 10 bold"),fg="black")
 
-re_passwd_entry=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="black")
-re_passwd_line=Canvas(frame2,width=260,height=2.0,bg="black",highlightthickness=0)
+var=IntVar
+c=Checkbutton(frame1,text="Keep Me Logged in",font=('Ariel', 17),variable=var,background="white")
+c.place(x=290,y=550)
 
-re_passwd_entry.place(x=340,y=330)
-re_passwd_line.place(x=325,y=354)
-re_passwd_label.place(x=300,y=310)
 
-    #PHONE NUMBER
-phn_label=Label(frame2,text="PHONE-NUMBER",bg="lavender",font=("Helvetica 10 bold"),fg="black")
+#canvas lines
+mycanvas=Canvas(frame1,width=1,height=450,background="skyblue")
+mycanvas.place(x=400,y=80)
 
-phn_entry=Entry(frame2,font=("Helvetica 14 bold"),bg="lavender",highlightthickness=0,relief=FLAT,fg="black")
-phn_line=Canvas(frame2,width=260,height=2.0,bg="black",highlightthickness=0)
+mycanvas2=Canvas(frame1,width=1,height=675,background="skyblue")
+mycanvas2.place(x=5,y=4)
 
-phn_entry.place(x=200,y=390)
-phn_line.place(x=190,y=414)
-phn_label.place(x=160,y=370)
+mycanvas3=Canvas(frame1,width=400,height=1,background="skyblue")
+mycanvas3.place(x=5,y=5)
+
+mycanvas4=Canvas(frame1,width=1,height=675,background="skyblue")
+mycanvas4.place(x=783,y=4)
+
+mycanvas5=Canvas(frame1,width=400,height=1,background="skyblue")
+mycanvas5.place(x=381,y=675)
+
+mycanvas6=Canvas(frame1,width=300,height=1,background="black")
+mycanvas6.place(x=50,y=200)
+
+mycanvas7=Canvas(frame1,width=300,height=1,background="black")
+mycanvas7.place(x=50,y=310)
+
+mycanvas8=Canvas(frame1,width=300,height=1,background="black")
+mycanvas8.place(x=50,y=420)
+
+mycanvas9=Canvas(frame1,width=300,height=1,background="black")
+mycanvas9.place(x=50,y=530)
+
+mycanvas10=Canvas(frame1,width=300,height=1,background="black")
+mycanvas10.place(x=450,y=200)
+
+mycanvas11=Canvas(frame1,width=300,height=1,background="black")
+mycanvas11.place(x=450,y=310)
+
+mycanvas12=Canvas(frame1,width=300,height=1,background="black")
+mycanvas12.place(x=450,y=420)
 
 
     #radio button to select gender of user
-gender=Label(frame2,text="GENDER",bg="lavender",fg="black",font=("Helvetica 10 bold"))
-gender.place(x=160,y=430)
+gender=Label(frame1,text="GENDER",font=('Ariel', 17),fg="skyblue",background="white")
+gender.place(x=445,y=440)
 gen = StringVar()
 gen.set(" ")
 
 def clicked():
     global gend
     gend=gen.get()
-Radiobutton(frame2,text="Male",fg="black",bg="lavender",font=("Helvetica 10 bold"),variable=gen,value="Male",command=clicked).place(x=200,y=460)
-Radiobutton(frame2,text="Female",fg="black",bg="lavender",font=("Helvetica 10 bold"),variable=gen,value="Female",command=clicked).place(x=280,y=460)
-Radiobutton(frame2,text="Other",fg="black",bg="lavender",font=("Helvetica 10 bold"),variable=gen,value="Other",command=clicked).place(x=380,y=460)
+Radiobutton(frame1,text="Male",font=('Ariel', 17),fg="skyblue",background="white",variable=gen,value="Male",command=clicked).place(x=415,y=495)
+Radiobutton(frame1,text="Female",font=('Ariel', 17),fg="skyblue",background="white",variable=gen,value="Female",command=clicked).place(x=520,y=495)
+Radiobutton(frame1,text="Other",font=('Ariel', 17),fg="skyblue",background="white",variable=gen,value="Other",command=clicked).place(x=650,y=495)
 
-
-def login_open():
-        reg.destroy()
-
-def open_login():                 #opens login page after registering account
-        reg.destroy()
-        import login
-
-#already a member
-text=Label(frame2,text="ALREADY A MEMBER?",font=("Helvetica 10 bold"),fg="red",bg="lavender")
-btn=Button(frame2,text="LOGIN",bg="lavender",fg="green",activebackground="lavender",bd=0,cursor="hand2",command=open_login)
-text.place(x=220,y=600)
-btn.place(x=365,y=600)
-
-    #===================================================================================================================================
+ #===================================================================================================================================
     #===================================================================================================================================
                                 #database connnection
                                 
@@ -130,12 +141,11 @@ btn.place(x=365,y=600)
 conn=sqlite3.connect('registration.db')
 c=conn.cursor()
 c.execute(""" CREATE TABLE IF NOT EXISTS register(
-        first_name text,
-        last_name text,
         user_name text PRIMARY KEY,
         password text,
-        email text ,
         phone_num integer,
+        email text,
+        ward no integer ,
         gender text
       
     )""")
@@ -143,14 +153,13 @@ c.execute(""" CREATE TABLE IF NOT EXISTS register(
 def add_rec():
     conn=sqlite3.connect('registration.db')
     c=conn.cursor()
-    c.execute("INSERT INTO register VALUES( :first_name, :last_name, :user_name, :password, :email, :phone_num, :gender )",{
+    c.execute("INSERT INTO register VALUES( :username, :password, :phone_num, :email, :ward,  :gender )",{
 
-            'first_name':first_name_ent.get(),
-            'last_name':lastname_ent.get(),
-            'user_name':un_ent.get(),
-            'password':passwd_ent.get(),
-            'email':email_ent.get(),
+            'username':usernam_entry.get(),
+            'password':pass_entry.get(),
             'phone_num':phn_entry.get(),
+            'email':entryemail.get(),
+            'ward':entryward.get(),
             'gender':gend
            
             })
@@ -159,17 +168,7 @@ def add_rec():
     messagebox.showinfo("Success",'account created Successfully!')
     open_login()
 
-#register button
-register=Button(frame2,text="REGISTER",fg="black",bg="limegreen",font=("Helvetica 15 bold"),activeforeground="limegreen",width=18,bd=0,cursor="hand2",command=add_rec)
-register.place(x=221,y=532)
-
-reg.mainloop()
-
-
-
-
-
-
-
-
-
+#REGISTER BUTTON
+loginbotton=Button(frame1,text="Create Account",font=('Ariel', 20),padx=250,pady=4.5,borderwidth=0,bg="skyblue",fg="white",command=add_rec)
+loginbotton.place(x=45,y=605)
+root.mainloop()
